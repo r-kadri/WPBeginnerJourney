5acd89b01d4b7afe4b72a47d2057fab02a969dd2
Ryan Kadri <ryankadri.dev@gmail.com>
2023-11-01 13:37:59 +0100

    :art: Start mytheme & include bootstrap

---
+++ wp-content/themes/mytheme/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+const BOOTSTRAP_CSS_URL = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
+const POPPER_JS_URL = 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js';
+const BOOTSTRAP_JS_URL = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js';
+
+function register_assets() {
+    wp_register_style('bootstrap', BOOTSTRAP_CSS_URL);
+    wp_register_script('bootstrap', BOOTSTRAP_JS_URL, ['popper'], false, ['in_footer' => true]);
+    wp_register_script('popper', POPPER_JS_URL, [], false, ['in_footer' => true]);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+add_action('after_setup_theme', function () {
+    add_theme_support('title-tag');
+});
+
+add_action('wp_enqueue_scripts', 'App\register_assets');
---
+++ wp-content/themes/mytheme/index.php
@@ -0,0 +1,10 @@
+<?php 
+get_header();
+?>
+
+<div class="container mt-5">
+    <h1>Hello world!</h1>
+</div>
+
+<?php
+get_footer();
